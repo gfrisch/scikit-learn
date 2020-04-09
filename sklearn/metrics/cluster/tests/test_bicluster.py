9be6994a950448c40483550f2405f43442b121e2
Gabriel Frisch <gabriel.frisch@hds.utc.fr>
2020-04-09 16:05:12 +0200

    CARI metric test

--- sklearn/metrics/cluster/tests/test_bicluster.py
+++ sklearn/metrics/cluster/tests/test_bicluster.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.utils._testing import assert_almost_equal
 
 from sklearn.metrics.cluster._bicluster import _jaccard
-from sklearn.metrics import consensus_score
+from sklearn.metrics import consensus_score, coclustering_adjusted_rand_score
 
 
 def test_jaccard():
@@ -21,8 +21,7 @@ def test_jaccard():
 
 
 def test_consensus_score():
-    a = [[True, True, False, False],
-         [False, False, True, True]]
+    a = [[True, True, False, False], [False, False, True, True]]
     b = a[::-1]
 
     assert consensus_score((a, a), (a, a)) == 1
@@ -37,14 +36,52 @@ def test_consensus_score():
 
 
 def test_consensus_score_issue2445():
-    ''' Different number of biclusters in A and B'''
-    a_rows = np.array([[True, True, False, False],
-                       [False, False, True, True],
-                       [False, False, False, True]])
-    a_cols = np.array([[True, True, False, False],
-                       [False, False, True, True],
-                       [False, False, False, True]])
+    """ Different number of biclusters in A and B"""
+    a_rows = np.array(
+        [
+            [True, True, False, False],
+            [False, False, True, True],
+            [False, False, False, True],
+        ]
+    )
+    a_cols = np.array(
+        [
+            [True, True, False, False],
+            [False, False, True, True],
+            [False, False, False, True],
+        ]
+    )
     idx = [0, 2]
     s = consensus_score((a_rows, a_cols), (a_rows[idx], a_cols[idx]))
     # B contains 2 of the 3 biclusters in A, so score should be 2/3
-    assert_almost_equal(s, 2.0/3.0)
+    assert_almost_equal(s, 2.0 / 3.0)
+
+
+def test_coclustering_adjusted_rand_score():
+    assert (
+        coclustering_adjusted_rand_score(
+            [0, 0, 1, 1], [0, 0, 1, 1, 2, 2], [0, 0, 1, 1], [0, 0, 1, 1, 2, 2]
+        )
+        == 1
+    )
+    assert (
+        coclustering_adjusted_rand_score(
+            [0, 0, 1, 1], [0, 0, 1, 1, 2, 2], [0, 0, 1, 1], [2, 2, 1, 1, 0, 0]
+        )
+        == 1
+    )
+    assert (
+        coclustering_adjusted_rand_score(
+            [0, 0, 0, 0], [0, 0, 0], [0, 1, 2, 3], [2, 1, 0]
+        )
+        == 0
+    )
+    assert_almost_equal(
+        coclustering_adjusted_rand_score(
+            [1, 2, 2, 2, 1],
+            [1, 1, 2, 1, 1, 2],
+            [1, 1, 2, 1, 1],
+            [1, 1, 2, 1, 3, 2],
+        ),
+        0.25005256149695143,
+    )
